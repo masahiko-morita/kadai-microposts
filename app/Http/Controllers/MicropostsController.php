ebcef9273150a40283c29f9711d2abe60898ec3e
masahiko.morita <masahiko.a.morita@rakuten.com>
2018-06-07 04:41:44 +0000

    7890

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -18,14 +18,15 @@ class MicropostsController extends Controller
         $data = [];
         if (\Auth::check()) {
             $user = \Auth::user();
-            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
 
             $data = [
                 'user' => $user,
                 'microposts' => $microposts,
             ];
-            return view('welcome', $data);
+            //$data += $this->counts($user);
         }
+         return view('welcome', $data);
     }
 
     /**
